8311009b8248828d4fc2254b7c107dcf53dd706f
Ashley Davis <ashley@codecapers.com.au>
2020-12-06 12:57:37 +1000

    Fixed config for publish.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 const withLess = require('@zeit/next-less')
 
 module.exports = withLess({
-    // assetPrefix: '/landing-page/',
-    // basePath: '/landing-page',
+    assetPrefix: '/landing-page/',
+    basePath: '/landing-page',
 
     lessLoaderOptions: {
         javascriptEnabled: true,
