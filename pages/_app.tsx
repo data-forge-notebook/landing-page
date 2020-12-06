b4e8b932725b6129f5af9b6edc68d61f2ab6dfd1
Ashley Davis <ashley@codecapers.com.au>
2020-12-06 11:45:24 +1000

    Converted global CSS to SASS.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router'
 import 'antd/dist/antd.css'
-import '../styles/globals.css'
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
