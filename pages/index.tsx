32de5d895b3fd07ec5d5969d95e32c42f1a56bf9
Ashley Davis <ashley@codecapers.com.au>
2020-12-07 15:33:58 +1000

    Main hero section.

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,41 +5,53 @@ export default function Home() {
   return (
     <Layout>
 
-      <main>
-          Hello!
+        <section className="hero">
+            <div className="container flex flex-col items-center">
+                <img className="logo" src="/images/logo.png" alt="Data-Forge Notebook logo" />
+                <div className="title">
+                    Exploratory coding and data analysis for JavaScript and TypeScript
+                </div>
+                <Link href="/product"><a className="action-button">Buy Data-Forge Notebook Pro!</a></Link>
+                <div className="vid-container">
+                    <iframe 
+                        width="560" 
+                        height="315" 
+                        src="https://www.youtube.com/embed/HO1J1G_A3d8" 
+                        frameBorder="0" 
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                        allowFullScreen
+                        >
+                    </iframe>
+                </div>
+                <a 
+                    className="mt-1"
+                    target="_blank" 
+                    href="https://www.youtube.com/channel/UCOxw0jy384_wFRwspgq7qMQ"
+                    >
+                    Watch more videos
+                </a>
+            </div>
+        </section>
 
-          <Link href="/product"><a>Buy Data-Forge Notebook Pro!</a></Link>
+        <section>
+            Features
+        </section>
 
-          <section>
-              Hero
+        <section>
+            Examples
+        </section>
 
-              <img
-                src="images/screenshots/Main.png" 
-                width={600}
-                />
-          </section>
+        <section>
+            Overview
+        </section>
 
-          <section>
-              Features
-          </section>
+        <section>
+            About
+        </section>
 
-          <section>
-              Examples
-          </section>
-
-          <section>
-              Overview
-          </section>
-
-          <section>
-              About
-          </section>
-
-          <section>
-              Contact
-          </section>
-
-      </main>
+        <section>
+            Contact
+        </section>
 
     </Layout>
   )
