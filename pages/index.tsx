da2edb59efe700c322e19897178761108c13d60d
Ashley Davis <ashley@codecapers.com.au>
2020-12-11 12:27:36 +1000

    Refactored zoomable image to reusable component.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,32 @@
 import Link from "next/link"
 import Layout from '../components/layout'
 
+function ZoomableImage({ src }: { src: string }) {
+    return (
+        <div className="zoomable-image">
+            <input type="checkbox" id="zoomCheck2" />
+            <label htmlFor="zoomCheck2">
+                <img 
+                    src={src} 
+                    alt="Screenshot" 
+                    className="screenshot"
+                    />
+            </label>
+            <div className="full-image-container">  
+                <label 
+                    htmlFor="zoomCheck2"
+                    className="full-image"
+                    >
+                    <img src={src} alt="Screenshot" />
+                </label>
+            </div>
+        </div>
+    );
+}
+
 export default function Home() {
   return (
     <Layout>
-
         <section className="blue">
             <div className="narrow-container flex flex-col items-center">
                 <img className="logo" src="/images/logo.png" alt="Data-Forge Notebook logo" />
@@ -39,46 +61,19 @@ export default function Home() {
                     <p className="text-2xl">Want to do visual prototyping and exploratory data analysis?</p>
                     <p className="mt-2 text-xl text-gray-600">But you need to work in JavaScript</p>
                 </div>
+
+                <ZoomableImage
+                    src="/images/screenshots/Screenshot_1.png"
+                    />
                 
-                <div className="zoomable-image">
-                    <input type="checkbox" id="zoomCheck2" />
-                    <label htmlFor="zoomCheck2">
-                        <img 
-                            src="/images/screenshots/Screenshot_1.png" 
-                            alt="Screenshot" 
-                            className="screenshot"
-                            />
-                    </label>
-                    <div className="full-image-container">  
-                        <label 
-                            htmlFor="zoomCheck2"
-                            className="full-image"
-                            >
-                            <img src="/images/screenshots/Screenshot_1.png" alt="Screenshot" />
-                        </label>
-                    </div>
-                </div>
             </div>
 
             <div className="wide-container flex flex-row items-center mt-10">
-                <div className="zoomable-image">
-                    <input type="checkbox" id="zoomCheck3" />
-                    <label htmlFor="zoomCheck3">
-                        <img 
-                            src="/images/screenshots/Screenshot_2.png" 
-                            alt="Screenshot" 
-                            className="screenshot"
-                            />
-                    </label>
-                    <div className="full-image-container">  
-                        <label 
-                            htmlFor="zoomCheck3"
-                            className="full-image"
-                            >
-                            <img src="/images/screenshots/Screenshot_2.png" alt="Screenshot" />
-                        </label>
-                    </div>
-                </div>
+
+               <ZoomableImage
+                    src="/images/screenshots/Screenshot_2.png"
+                    />
+
                 <div className="flex-grow pl-4">
                     <p className="text-2xl">Love the idea of Notebook-style or literate programing?</p>
                     <p className="mt-2 text-xl text-gray-600">But your production environment is JavaScript</p>
@@ -91,24 +86,11 @@ export default function Home() {
                     <p className="mt-2 text-xl text-gray-600">Data-Forge Notebook offers a friendly and forgiving environment that includes Node.js, automatically installs npm modules, has zero configuration and just works.</p>
                     <p className="mt-2 text-xl text-gray-600">Just fire it up and start coding</p>
                 </div>
-                <div className="zoomable-image">
-                    <input type="checkbox" id="zoomCheck4" />
-                    <label htmlFor="zoomCheck4">
-                        <img 
-                            src="/images/screenshots/Screenshot_3.png" 
-                            alt="Screenshot" 
-                            className="screenshot"
-                            />
-                    </label>
-                    <div className="full-image-container">  
-                        <label 
-                            htmlFor="zoomCheck4"
-                            className="full-image"
-                            >
-                            <img src="/images/screenshots/Screenshot_3.png" alt="Screenshot" />
-                        </label>
-                    </div>
-                </div>
+
+                <ZoomableImage
+                    src="/images/screenshots/Screenshot_3.png"
+                    />
+
             </div>
         </section>
 
